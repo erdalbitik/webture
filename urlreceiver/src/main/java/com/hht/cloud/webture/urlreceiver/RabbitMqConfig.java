0628ca1f3d8a491761f48c0a4673075c408817a2
Erdal Bitik <erdalbitik@gmail.com>
2018-09-17 22:36:46 +0300

    rabbit mq

---
+++ urlreceiver/src/main/java/com/hht/cloud/webture/urlreceiver/RabbitMqConfig.java
@@ -0,0 +1,58 @@
+package com.hht.cloud.webture.urlreceiver;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
+@Configuration
+public class RabbitMqConfig implements RabbitListenerConfigurer {
+
+	@Bean
+	Queue dataQueue() {
+		return new Queue("data-queue", false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange("webture:fileProcess");
+	}
+
+	@Bean
+	Binding dataBinding(@Qualifier("dataQueue") Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("data");
+	}
+
+	@Bean
+	MappingJackson2MessageConverter jackson2Converter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		return converter;
+	}
+
+	/**
+	 * give Jackson ObjetMapper to RabbitMq for JSON Mapping
+	 *
+	 * @return
+	 */
+	@Bean
+	DefaultMessageHandlerMethodFactory jsonMessageHandlerMethod() {
+		DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+		factory.setMessageConverter(jackson2Converter());
+
+		return factory;
+	}
+
+	@Override
+	public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
+		rabbitListenerEndpointRegistrar.setMessageHandlerMethodFactory(jsonMessageHandlerMethod());
+	}
+
+
+}
