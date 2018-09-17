a5d79263e89512380070bfa399087a8131484470
Erdal Bitik <erdalbitik@gmail.com>
2018-09-17 21:47:24 +0300

    rabbitmq

--- urlreceiver/src/main/java/com/hht/cloud/webture/urlreceiver/controller/FileProcessController.java
+++ urlreceiver/src/main/java/com/hht/cloud/webture/urlreceiver/controller/FileProcessController.java
@@ -3,7 +3,11 @@ package com.hht.cloud.webture.urlreceiver.controller;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hht.cloud.webture.urlreceiver.model.FileProcess;
 import com.hht.cloud.webture.urlreceiver.repository.FileProcessRepository;
+import com.hht.cloud.webture.urlreceiver.service.MessageService;
 
 @RestController
 public class FileProcessController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileProcessController.class);
+	
 	@Autowired
 	private FileProcessRepository repository;
+	
+	@Autowired
+	private MessageService messageService;
 	
 	@GetMapping("/")
     public List<FileProcess> list() {
@@ -24,13 +34,19 @@ public class FileProcessController {
     }
 	
 	@PostMapping("/")
-    public FileProcess addUrl(@RequestBody UrlDTO dto) {
+    public ResponseEntity<FileProcess> addUrl(@RequestBody UrlDTO dto) {
 		FileProcess fp = new FileProcess();
 		fp.setCaptureUrl(dto.getUrl());
 		fp.setCreateDate(new Date());
 		fp.setFileType(dto.getFileType());
-		FileProcess insert = repository.insert(fp);
-        return insert;
+		fp = repository.insert(fp);
+		boolean sentToQueue = messageService.sendQueueMessage(fp);
+		if(!sentToQueue) {
+			repository.delete(fp);
+			LOGGER.info("Message rollbacked!");
+			return new ResponseEntity<FileProcess>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+        return new ResponseEntity<FileProcess>(fp, HttpStatus.OK);
     }
 
 
